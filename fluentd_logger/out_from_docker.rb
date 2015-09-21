ea99c9b4689bed6b14304c7b4067a1d1c682ec0f
dlorenc <lorenc.d@gmail.com>
2015-09-21 15:19:49 -0700

    Move the fluentd_logger config and build scripts into git.
    Change-Id: Ie59a922c2a479a3a0c66b99fa5470429bc246cea

---
+++ fluentd_logger/out_from_docker.rb
@@ -0,0 +1,43 @@
+# Copyright 2015 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+module Fluent
+  # Logs from docker style logs to GoogleCloudOutput style logs
+  class FromDockerOutput < Output
+    include Configurable
+
+    # The tag to output stdout logs
+    config_param :stdout_tag, :string, :default => 'stdout'
+    # The tag to output stderr logs
+    config_param :stderr_tag, :string, :default => 'stderr'
+
+    Fluent::Plugin.register_output('from_docker', self)
+
+    def emit(tag, chunk, next_chain)
+      chunk.each do |time_sec,record|
+        time = Time.at(time_sec)
+        timestamp = {'seconds' => time.tv_sec, 'nanos' => time.tv_nsec}
+        message = record['log']
+        new_record = {'timestamp' => timestamp, 'message' => message}
+        new_tag = stdout_tag
+        if record['stream'] == 'stderr'
+          new_tag = stderr_tag
+        end
+        Engine.emit(new_tag, time_sec, new_record)
+      end
+      next_chain.next
+    end
+  end
+end
