93625b28a24e89242aff8ce6848e67818325ead1
Guillaume Binet <gbin@google.com>
2015-10-13 10:56:21 -0700

    take a less destructive approach on internal.

--- memcache_proxy/build.sh
+++ memcache_proxy/build.sh
@@ -4,12 +4,16 @@ export GOPATH=$(mktemp --directory)
 mkdir -p $GOPATH/src $GOPATH/pkg
 
 go get google.golang.org/appengine/internal
-sed -i 's|instance/attributes/gae_minor_version|instance/attributes/gae_backend_minor_version|g' $GOPATH/src/google.golang.org/appengine/internal/identity_vm.go
 cp -R $GOPATH/src/google.golang.org/appengine/internal $GOPATH/src/google.golang.org/appengine/notreallyinternal
+sed -i 's|instance/attributes/gae_minor_version|instance/attributes/gae_backend_minor_version|g' $GOPATH/src/google.golang.org/appengine/notreallyinternal/identity_vm.go
 
 DEST=$GOPATH/src
+
+echo "Building in $DEST"
+
 mkdir -p $DEST/dtog/main
 cp dtog.go $DEST/dtog
+cp dtog_test.go $DEST/dtog
 cp main/memcached2g.go $DEST/dtog/main
 
 go build -o memcachep main/memcached2g.go
