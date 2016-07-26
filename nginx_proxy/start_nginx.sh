df6a612fb7f723606d907f4eb7bda2b7308a85f0
Sep Ebrahimzadeh <sepehr@google.com>
2016-07-26 11:48:29 -0700

    Update the nginx Docker image.
    1. Install cron so that log rotation can work.
    2. Optionally accept Service Name and Service Version as parameters for
    Endpoints Server Proxy.

--- nginx_proxy/start_nginx.sh
+++ nginx_proxy/start_nginx.sh
@@ -18,6 +18,43 @@ KEY_FILE=${CERT_DIR}/lb.key
 CSR_FILE=${CERT_DIR}/lb.csr
 CRT_FILE=${CERT_DIR}/lb.crt
 
+SERVICE_NAME=''
+SERVICE_VERSION=''
+
+usage () {
+  cat << END_USAGE
+Usage: $(command basename $0) [options]
+Examples:
+(1) Starts nginx with no Endpoints service name and service version. In this
+mode either Endpoints features are disabled via nginx.conf or the service
+configuration is already provided on disk:
+$(command basename $0)
+(2) Starts nginx with a custom Endpoints service name and service version, and
+have nginx obtain the service configuration:
+$(command basename $0) -n SERVICE_NAME -v SERVICE_VERSION
+Options:
+    -h
+        Shows this message.
+    -n ENDPOINTS_SERVICE_NAME
+        Required. The name of the Endpoints Service.
+        e.g. my-service.my-project-id.appspot.com
+    -v ENDPOINTS_SERVICE_VERSION
+        Required. The version of the Endpoints Service which is assigned
+        when deploying the service API specification.
+        e.g. 2016-04-20R662
+dd    -
+END_USAGE
+  exit 1
+}
+while getopts 'ha:n:N:p:S:s:v:' arg; do
+  case ${arg} in
+    h) usage;;
+    n) SERVICE_NAME="${OPTARG}";;
+    v) SERVICE_VERSION="${OPTARG}";;
+    ?) usage;;
+  esac
+done
+
 mkdir -p ${CERT_DIR}
 if [[ ! -f "${KEY_FILE}" ]]; then
   rm -f ${CSR_FILE} ${CRT_FILE}
@@ -40,6 +77,13 @@ fi
 # use the override nginx.conf if there is one.
 if [[ -f "${CONF_FILE}" ]]; then
   cp "${CONF_FILE}" /etc/nginx/nginx.conf
+fi
+
+# fetch Service Configuration from Service Management if the service name and
+# service version are provided.
+if [[ -n "${SERVICE_NAME}" && -n "${SERVICE_VERSION}" ]]; then
+  /usr/sbin/fetch_service_config.sh \
+    -s "${SERVICE_NAME}" -v "${SERVICE_VERSION}" || exit $?
 fi
 
 /usr/sbin/nginx -p /usr -c /etc/nginx/nginx.conf
