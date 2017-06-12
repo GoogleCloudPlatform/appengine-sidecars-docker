94d3869ed83003f2c746a28cdd5f160dec23025a
Tariq Yusuf <yusuft@google.com>
2017-06-12 19:18:59 -0400

    Refactor main script into function to make testing easier.

---
+++ iap_verifier/iap_verifier_test.py
@@ -0,0 +1,20 @@
+import tempfile
+import unittest
+import iap_verifier
+
+class TestIapVerifier(unittest.TestCase):
+
+	def setUpModule(self):
+		self.handler_, self.pathname_ = tempfile.mkstemp()
+
+	def tearDownClass(self):
+		self.handler_.close()
+
+	def testKeyFile(self):
+		iap_verifier.Main({
+			'key': 'AEF_IAP_state',
+			'output_state_file': cls.pathname_,
+		})
+
+if __name__ == '__main__':
+	unittest.main()
