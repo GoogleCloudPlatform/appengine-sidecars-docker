2e9d783b229b578517ee4af5f5db3f99d1c077ad
Jae Kim <jaebong@google.com>
2018-05-08 09:24:40 -0700

    Rename the variable

--- nginx_proxy/start_nginx.sh
+++ nginx_proxy/start_nginx.sh
@@ -21,7 +21,7 @@ readonly CRT_FILE=${CERT_DIR}/lb.crt
 ENDPOINTS_SERVICE_NAME=''
 ENDPOINTS_SERVICE_VERSION=''
 ENDPOINTS_ROLLOUT_STRATEGY='fixed'
-ENDPOINTS_CLOUD_TRACE_FLAG='true'
+ENDPOINTS_CLOUD_TRACE_AUTO_SAMPLING_FLAG='true'
 
 usage () {
   cat << END_USAGE
@@ -57,9 +57,9 @@ Options:
         specified in the latest rollout. If it is not specified, "fixed"
         would be chosen by default.
         e.g. fixed
-    -t ENDPOINTS_CLOUD_TRACE_FLAG
-        Optional. Enable or disable cloud trace. The value should be either
-        "true" or "false". The default value is "true".
+    -t ENDPOINTS_CLOUD_TRACE_AUTO_SAMPLING_FLAG
+        Optional. Enable or disable cloud trace auto sampling. The value
+        should be either "true" or "false". The default value is "true".
 END_USAGE
   exit 1
 }
@@ -69,7 +69,7 @@ while getopts 'ha:n:N:p:S:s:v:r:t:' arg; do
     n) ENDPOINTS_SERVICE_NAME="${OPTARG}";;
     v) ENDPOINTS_SERVICE_VERSION="${OPTARG}";;
     r) ENDPOINTS_ROLLOUT_STRATEGY="${OPTARG}";;
-    t) ENDPOINTS_CLOUD_TRACE_FLAG="${OPTARG}";;
+    t) ENDPOINTS_CLOUD_TRACE_AUTO_SAMPLING_FLAG="${OPTARG}";;
     ?) usage;;
   esac
 done
@@ -126,8 +126,8 @@ if [[ "${ENDPOINTS_ROLLOUT_STRATEGY}" == 'managed' && \
   usage
 fi
 
-if [[ "${ENDPOINTS_CLOUD_TRACE_FLAG}" != 'true' && \
-      "${ENDPOINTS_CLOUD_TRACE_FLAG}" != 'false' ]]; then
+if [[ "${ENDPOINTS_CLOUD_TRACE_AUTO_SAMPLING_FLAG}" != 'true' && \
+      "${ENDPOINTS_CLOUD_TRACE_AUTO_SAMPLING_FLAG}" != 'false' ]]; then
   echo 'Error: invalid value for cloud trace flag'
   usage
 fi
@@ -142,8 +142,8 @@ fi
 if [[ "${ENDPOINTS_ROLLOUT_STRATEGY}" ]]; then
   cmd+=" --rollout_strategy \"${ENDPOINTS_ROLLOUT_STRATEGY}\""
 fi
-if [[ "${ENDPOINTS_CLOUD_TRACE_FLAG}" == 'false' ]]; then
-  cmd+=" --disable_cloud_trace_sampling"
+if [[ "${ENDPOINTS_CLOUD_TRACE_AUTO_SAMPLING_FLAG}" == 'false' ]]; then
+  cmd+=" --disable_cloud_trace_auto_sampling"
 fi
 cmd+=" --client_ip_header \"X-Forwarded-For\""
 cmd+=" --client_ip_position -2"
