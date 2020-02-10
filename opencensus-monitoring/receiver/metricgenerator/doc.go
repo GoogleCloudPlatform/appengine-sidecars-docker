a4075294c6384a22c399f06c7c7d0b1f287ba6dc
Isabel McCarten <imccarten@google.com>
2020-02-10 14:57:14 -0800

    Add package doc comments for vmimageagereceiver and metricgenerator.

--- opencensus-monitoring/receiver/metricgenerator/doc.go
+++ opencensus-monitoring/receiver/metricgenerator/doc.go
@@ -1,1 +1,4 @@
+// Package metricgenerator provides utility functions intended to be used by
+// metric receivers the generate metrics rather taking in existing metrics from
+// an external source.
 package metricgenerator
