d211e9f0e840703a8fe114ab6f54cffc019b672d
Isabel McCarten <imccarten@google.com>
2020-02-19 17:33:24 -0800

    Add the resourceprocessor to components.
    This allows setting the monitored resource using a processor.
    Also updates metrics_receiver to match the new receiver interface.

--- opentelemetry_collector/go.mod
+++ opentelemetry_collector/go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/GoogleCloudPlatform/appengine-sidecars-docker v0.0.0-20191008180841-97c09a40bf9b // indirect
 	github.com/census-instrumentation/opencensus-proto v0.2.1
 	github.com/golang/protobuf v1.3.2
-	github.com/open-telemetry/opentelemetry-collector v0.2.1-0.20191016224815-dfabfb0c1d1e
+	github.com/open-telemetry/opentelemetry-collector v0.2.6
 	github.com/open-telemetry/opentelemetry-collector-contrib/exporter/stackdriverexporter v0.0.0-20191203223618-d7af801adf6e
 	github.com/stretchr/testify v1.4.0
 	go.uber.org/zap v1.13.0
--- opentelemetry_collector/receiver/vmimageagereceiver/metrics_receiver.go
+++ opentelemetry_collector/receiver/vmimageagereceiver/metrics_receiver.go
@@ -3,7 +3,7 @@ package vmimageagereceiver
 import (
 	"sync"
 
-	"github.com/open-telemetry/opentelemetry-collector/receiver"
+	"github.com/open-telemetry/opentelemetry-collector/component"
 )
 
 // Receiver is the type that provides Receiver functionaly for the VM image age metrics.
@@ -14,15 +14,8 @@ type Receiver struct {
 	startOnce sync.Once
 }
 
-const metricsSource string = "VMImageAgeReceiver"
-
-// MetricsSource gets the receiver type.
-func (receiver *Receiver) MetricsSource() string {
-	return metricsSource
-}
-
 // StartMetricsReception starts the underlying VM metrics generator.
-func (receiver *Receiver) StartMetricsReception(host receiver.Host) error {
+func (receiver *Receiver) Start(host component.Host) error {
 	receiver.startOnce.Do(func() {
 		receiver.vmImageAgeCollector.StartCollection()
 	})
@@ -30,7 +23,7 @@ func (receiver *Receiver) StartMetricsReception(host receiver.Host) error {
 }
 
 // StopMetricsReception stops and cancels the underlying VM metrics generator.
-func (receiver *Receiver) StopMetricsReception() error {
+func (receiver *Receiver) Shutdown() error {
 	receiver.stopOnce.Do(func() {
 		receiver.vmImageAgeCollector.StopCollection()
 	})
