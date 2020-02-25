fe409911fb8a4e792a5b0a01513cf80c28ce2ccc
Isabel McCarten <imccarten@google.com>
2020-02-25 14:44:07 -0800

    Change the run script to use env variables rather than querying metadata

--- opentelemetry_collector/run.sh
+++ opentelemetry_collector/run.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 BUILD_DATE_FILE=/config/build_date.txt
-METADATA_FILE=/tmp/metadata.txt
 OPENTELEMETRY_CONFIG_FILE=/opentelemetry_config.yaml
-METADATA_URL="http://metadata.google.internal/computeMetadata/v1/instance"
 
-get_metadata () {
-  local METADATA_PATH="${1}"
+set_metadata () {
+  local METADATA_VAR="${1}"
   local REPLACE_NAME="${2}"
 
-  local STATUS_CODE=$(curl --write-out %{http_code} --silent --output "${METADATA_FILE}" -H "Metadata-Flavor: Google" "${METADATA_URL}/${METADATA_PATH}")
-  if [[ "${STATUS_CODE}" -eq 200 ]]; then
-    sed -i "s!@${REPLACE_NAME}@!$(cat ${METADATA_FILE})!" "${OPENTELEMETRY_CONFIG_FILE}"
-  else
+  if [[ -z "${METADATA_VAR}" ]]; then
     sed -i "s/@${REPLACE_NAME}@/unknown/" "${OPENTELEMETRY_CONFIG_FILE}"
+  else
+    sed -i "s!@${REPLACE_NAME}@!${METADATA_VAR}!" "${OPENTELEMETRY_CONFIG_FILE}"
   fi
 }
 
@@ -23,18 +20,17 @@ else
   sed -i "s/@BUILD_DATE@/unknown/" "${OPENTELEMETRY_CONFIG_FILE}"
 fi
 
-get_metadata "image" "IMAGE_NAME"
-get_metadata "attributes/gae_backend_version" "VERSION"
-get_metadata "attributes/gae_backend_name" "SERVICE"
-get_metadata "name" "INSTANCE"
+set_metadata "${IMAGE}" "IMAGE_NAME"
+set_metadata "${GAE_BACKEND_VERSION}" "VERSION"
+set_metadata "${GAE_BACKEND_NAME}" "SERVICE"
+set_metadata "${INSTANCE_ID}" "INSTANCE"
 
-STATUS_CODE=$(curl --write-out %{http_code} --silent --output "${METADATA_FILE}" -H "Metadata-Flavor: Google" "${METADATA_URL}/zone")
-if [[ "${STATUS_CODE}" -eq 200 ]]; then
-  sed -i 's!.*zones/!!' "${METADATA_FILE}"
-  sed -i 's/-.$//' "${METADATA_FILE}"
-  sed -i "s!@REGION@!$(cat ${METADATA_FILE})!" "${OPENTELEMETRY_CONFIG_FILE}"
-else
+if [[ -z "${ZONE}" ]]; then
   sed -i "s/@REGION@/unknown/" "${OPENTELEMETRY_CONFIG_FILE}"
+else
+  FORMATTED_ZONE=$(echo "${ZONE}" | sed 's!.*zones/!!')
+  REGION=$(echo "${FORMATTED_ZONE}" | sed 's/-.$//')
+  sed -i "s!@REGION@!${REGION}!" "${OPENTELEMETRY_CONFIG_FILE}"
 fi
 
 /opentelemetry_collector --config="${OPENTELEMETRY_CONFIG_FILE}"
